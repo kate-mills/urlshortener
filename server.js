df082d31d2031cc3b1ab35fb809203c4c40a8ddf
Kate Mills <helloworldloveksm@icloud.com>
2022-04-06 15:35:59 -0700

    create and get shorturl

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const urlHandler = new (require('./shortUrlHandler'))()
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(cors());
+// handle urlencoded data
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(cors())
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use('/public', express.static(`${process.cwd()}/public`))
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.post('/api/shorturl', (req, res) => {
+  let { url } = req.body
+
+  if (!url) return res.json({ error: 'invalid url' })
+
+  let { passed } = urlHandler.testShortUrl(url)
+
+  if (!passed) {
+    return res.json({ error: 'invalid url' })
+  }
+
+  let { original_url, short_url } = urlHandler.createOrUpdateUrl(url)
+
+  return res.json({ original_url, short_url })
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
---
+++ shorturlHandler.js
@@ -0,0 +1,35 @@
+function ShortUrlHandler() {
+  this.db = []
+
+  this.testShortUrl = (url) => {
+    let regex = /^(http|https):\/\/[^ "]+$/i
+    return { passed: regex.test(url) }
+  }
+
+  this.getSafeUrl = (url) => {
+    let regex = /^(http|https):\/\/[^ "]+$/i
+    let [matched, ...rest] = url.match(regex)
+    if (matched[matched.length - 1] == '/') {
+      matched = matched.slice(0, matched.length - 1)
+      return matched
+    }
+    return url
+  }
+
+  this.createOrUpdateUrl = (url) => {
+    url = url.toLowerCase().trim()
+    url = this.getSafeUrl(url)
+
+    if (this.db[url]) {
+      let { id } = this.db[url]
+      return { short_url: id, original_url: url }
+    }
+    let id = this.db.length + 100
+    this.db[url] = { url, id }
+    this.db.push(this.db[url])
+
+    return { short_url: id, original_url: url }
+  }
+}
+
+module.exports = ShortUrlHandler
